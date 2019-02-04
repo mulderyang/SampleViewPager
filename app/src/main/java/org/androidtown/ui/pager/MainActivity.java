ab36c6c54d1151293913b95d636711fdf64d1011
jeny <iblind@naver.com>
2019-02-04 13:34:21 +0900

    ViewPager 에 OnPageChangeListener( ) 추가.

--- app/src/main/java/org/androidtown/ui/pager/MainActivity.java
+++ app/src/main/java/org/androidtown/ui/pager/MainActivity.java
@@ -68,6 +68,23 @@ public class MainActivity extends AppCompatActivity {
         ViewPagerAdapter adapter = new ViewPagerAdapter(this);
         pager.setAdapter(adapter);
 
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
 
 
 
